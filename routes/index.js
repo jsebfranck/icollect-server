cdac8e982c475a8cb58f8ec3bcae4f3a0f7ef28f
Jean-Sébastien Franck <jsebfranck@gmail.com>
2014-09-03 11:13:25 +0200

    First route to post collects

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.json({ 'welcome': true });
+router.post('/collect', function(req, res) {
+  res.json({
+    'collectId': '1234IJEIOJHFEL'
+  });
 });
 
 module.exports = router;

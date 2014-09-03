101e7cad710bbfe04cba1fc83bd8ccc20f8b713e
Jean-Sébastien Franck <jsebfranck@gmail.com>
2014-09-03 11:41:54 +0200

    Login service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express'),
   router = express.Router(),
-  service = require('../services/collect.service');
+  collectService = require('../services/collect.service'),
+  loginService = require('../services/login.service');
+
+router.post('/login', function(req, res) {
+  loginService.login(req.body).then(function(result) {
+    res.json(result);
+  });
+});
 
 router.post('/collect', function(req, res) {
-  service.createCollect(req.body).then(function(result) {
+  collectService.createCollect(req.body).then(function(result) {
     res.json(result);
   });
 });

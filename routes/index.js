c0f0f12b66aa6fc2795f43bb33f4ec86a04522c4
Jean-Sébastien Franck <jsebfranck@gmail.com>
2014-09-03 14:38:37 +0200

    Mock data for collect and login services

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,27 @@ var express = require('express'),
   loginService = require('../services/login.service');
 
 router.post('/login', function(req, res) {
+  res.json({
+    id:'JKLMQSDFJMLKEZMKL',
+    lastname: 'Collex',
+    firstname: 'Tatiana',
+    role: 'IN_CHARGE'
+  });
+});
+
+router.post('/collect', function(req, res) {
+  res.json({
+    status: 'ok';
+  });
+});
+
+router.post('/login-backend', function(req, res) {
   loginService.login(req.body).then(function(result) {
     res.json(result);
   });
 });
 
-router.post('/collect', function(req, res) {
+router.post('/collect-backend', function(req, res) {
   collectService.createCollect(req.body).then(function(result) {
     res.json(result);
   });

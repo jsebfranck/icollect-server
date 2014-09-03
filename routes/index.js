2253dea9c100fc8394a67e073f76844dfee4a532
Jean-Sébastien Franck <jsebfranck@gmail.com>
2014-09-03 14:55:26 +0200

    Login refactoring

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,14 @@ var express = require('express'),
   loginService = require('../services/login.service');
 
 router.post('/login', function(req, res) {
-  res.json({
-    id:'JKLMQSDFJMLKEZMKL',
-    lastname: 'Collex',
-    firstname: 'Tatiana',
-    role: 'IN_CHARGE'
+  loginService.login(req.body).then(function(result) {
+    res.json(result);
   });
 });
 
 router.post('/collect', function(req, res) {
   res.json({
     status: 'ok'
-  });
-});
-
-router.post('/login-backend', function(req, res) {
-  loginService.login(req.body).then(function(result) {
-    res.json(result);
   });
 });
 
